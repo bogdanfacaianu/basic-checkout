91a08c11470e5f85db0312744173cf1af47e2e1a
bogdanfacaianu <bogdy.facaianu@gmail.com>
2021-03-12 01:26:36 +0000

    updated stock

--- src/com/basic/checkout/stock/Stock.java
+++ src/com/basic/checkout/stock/Stock.java
@@ -1,14 +1,16 @@
 package com.basic.checkout.stock;
 
+import com.basic.checkout.checkout.ScannedItem;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
-public class Stock {
+public final class Stock {
 
     private static Stock instance;
-    private final Map<String, Sku> stock;
+    private final Map<String, StockItem> stock;
 
     private Stock() {
         stock = new HashMap<>();
@@ -18,24 +20,29 @@ public class Stock {
         return instance == null ? new Stock() : instance;
     }
 
-    public void loadBulkStock(Collection<Sku> stock) {
+    public void loadBulkStock(Collection<StockItem> stock) {
         stock.forEach(this::addSku);
     }
 
-    public void addSku(Sku sku) {
-
+    public void addSku(StockItem sku) {
+        this.stock.put(sku.getSkuId(), sku);
     }
 
-    private Sku getSku(String skuId) {
-        return null;
+    public void addStockOffer(String skuId, Offer offer) {
+        if (stock.containsKey(skuId)) {
+            stock.get(skuId).applyOffer(offer);
+        }
     }
 
-    private void removeSku(String skuId) {
-
+    public boolean isStockEmpty() {
+        return stock.isEmpty();
     }
 
-    private void clearStock() {
-
+    public Optional<Offer> getOfferForSku(String skuId) {
+        if (this.stock.containsKey(skuId)) {
+            return Optional.ofNullable(stock.get(skuId).getOffer());
+        }
+        return Optional.empty();
     }
 
     @Override
@@ -55,10 +62,46 @@ public class Stock {
         return Objects.hash(stock);
     }
 
-    @Override
-    public String toString() {
-        return "Stock{" +
-            "stock=" + stock +
-            '}';
+    public Optional<ScannedItem> decorateStockItem(String scannedSku, ScannedItem previousScan) {
+        if (stock.containsKey(scannedSku)) {
+            StockItem stockItem = this.stock.get(scannedSku);
+            return buildUpdatedScannedItem(stockItem, previousScan);
+        }
+        return Optional.empty();
+    }
+
+    private Optional<ScannedItem> buildUpdatedScannedItem(StockItem stockItem, ScannedItem previousScan) {
+        Offer stockOffer = stockItem.getOffer();
+        ScannedItem scannedItem = new ScannedItem(stockItem.getSkuId(), stockItem.getPrice());
+        if (previousScan != null) {
+            scannedItem.setQuantity(previousScan.getQuantity() + 1);
+            updateTotalCost(scannedItem, stockOffer);
+        } else {
+            scannedItem.setQuantity(1);
+            scannedItem.setTotalCost(stockItem.getPrice());
+        }
+        return Optional.of(scannedItem);
+    }
+
+    private void updateTotalCost(ScannedItem scannedItem, Offer itemOffer) {
+        if (itemOffer != null) {
+            if (scannedItem.getQuantity() < itemOffer.getMultiplier()) {
+                double totalCost = scannedItem.getQuantity() * scannedItem.getPrice();
+                scannedItem.setTotalCost(totalCost);
+            } else {
+                applyTotalWithDiscount(scannedItem, itemOffer);
+            }
+        } else {
+            double totalCost = scannedItem.getQuantity() * scannedItem.getPrice();
+            scannedItem.setTotalCost(totalCost);
+        }
+    }
+
+    private void applyTotalWithDiscount(ScannedItem scannedItem, Offer itemOffer) {
+        double totalCost = ((scannedItem.getQuantity() / itemOffer.getMultiplier()) * itemOffer.getOfferPrice())
+            + ((scannedItem.getQuantity() % itemOffer.getMultiplier()) * scannedItem.getPrice());
+
+        scannedItem.setTotalCost(totalCost);
+        scannedItem.setDiscounted(true);
     }
 }
