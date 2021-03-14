5f58f572bbb4c2c48c22dd885fda488a8c3c42f9
bogdanfacaianu <bogdy.facaianu@gmail.com>
2021-03-14 01:19:29 +0000

    simulator action interface

--- src/com/basic/checkout/simulator/output/ActionPrinter.java
+++ src/com/basic/checkout/simulator/output/ActionPrinter.java
@@ -1,2 +1,12 @@
-package com.basic.checkout.simulator.output;public interface ActionPrinter {
+package com.basic.checkout.simulator.output;
+
+import com.basic.checkout.checkout.ShoppingBasket;
+import com.basic.checkout.simulator.InputHandler;
+import com.basic.checkout.store.Store;
+
+public interface ActionPrinter {
+
+    void print(Store store, ShoppingBasket shoppingBasket);
+
+    int printReadMenuOption(InputHandler inputHandler);
 }
