5f418c6c6c59a7bc77385439338eeb9637519d4a
bogdanfacaianu <bogdy.facaianu@gmail.com>
2021-03-14 01:20:57 +0000

    checkout cli simulator

--- src/com/basic/checkout/simulator/CheckoutSimulator.java
+++ src/com/basic/checkout/simulator/CheckoutSimulator.java
@@ -1,12 +1,16 @@
 package com.basic.checkout.simulator;
 
-import com.basic.checkout.checkout.StockTransaction;
-import com.basic.checkout.store.Store;
 import com.basic.checkout.checkout.ShoppingBasket;
+import com.basic.checkout.checkout.StockTransaction;
 import com.basic.checkout.checkout.TransactionManager;
+import com.basic.checkout.simulator.output.PrintBasketActions;
+import com.basic.checkout.simulator.output.PrintStockActions;
+import com.basic.checkout.sku.Offer;
+import com.basic.checkout.sku.StockItem;
+import com.basic.checkout.store.Store;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
-import java.util.Scanner;
+import javafx.util.Pair;
 
 public class CheckoutSimulator implements Simulator {
 
@@ -16,13 +20,20 @@ public class CheckoutSimulator implements Simulator {
     private final Store store;
     private final ShoppingBasket shoppingBasket;
 
-    private final Scanner input;
+    private final PrintBasketActions printBasketActions;
+    private final PrintStockActions printStockActions;
+
+    private final InputHandler inputHandler;
 
     public CheckoutSimulator() {
         store = Store.openShop();
         stockTransaction = new TransactionManager();
         shoppingBasket = new ShoppingBasket();
-        input = new Scanner(System.in);
+
+        inputHandler = new InputHandler();
+
+        printBasketActions = new PrintBasketActions();
+        printStockActions = new PrintStockActions();
     }
 
     @Override
@@ -31,42 +42,89 @@ public class CheckoutSimulator implements Simulator {
     }
 
     private void promptFirstMenu() {
-
+        printStockActions.print(store, shoppingBasket);
+        int chosenMenuOption = printStockActions.printReadMenuOption(inputHandler);
+        pickNextStockPath(chosenMenuOption);
     }
 
-    private void promptStockOptionsMenu() {
-
+    private void promptCheckoutMenu() {
+        printBasketActions.print(store, shoppingBasket);
+        int chosenMenuOption = printBasketActions.printReadMenuOption(inputHandler);
+        pickNextCheckoutPath(chosenMenuOption);
     }
 
-    private void promptScanProducts() {
-
+    private void pickNextStockPath(int chosenMenuOption) {
+        clearScreen();
+        switch (chosenMenuOption) {
+            case 1:
+                loadDefaultStock();
+                break;
+            case 2:
+                StockItem item = printStockActions.printReadStockValue(inputHandler);
+                stockTransaction.addStock(store, item);
+                break;
+            case 3:
+                Pair<String, Offer> readValues = printStockActions.printReadOffer(inputHandler, store);
+                stockTransaction.addStockOffer(store, readValues.getKey(), readValues.getValue());
+                break;
+            case 4:
+                promptCheckoutMenu();
+                break;
+            default:
+                break;
+        }
+        promptFirstMenu();
     }
 
-    private int validateAndReturnChosenInputOption() {
-        return 0;
+    private void pickNextCheckoutPath(int chosenMenuOption) {
+        clearScreen();
+        switch (chosenMenuOption) {
+            case 1:
+                String scannedSku = printStockActions.printScanSku(inputHandler, store);
+                stockTransaction.scanItem(store, shoppingBasket, scannedSku);
+                break;
+            case 2:
+                printBasketActions.print(store, shoppingBasket);
+                System.out.println("Please take your receipt!");
+                System.exit(1);
+            case 3:
+                shoppingBasket.clear();
+                break;
+            case 4:
+                System.out.println("Transaction cancelled!");
+                System.exit(1);
+            default:
+                break;
+        }
+        promptCheckoutMenu();
     }
 
-    private String validateAndUseScannedProduct() {
-        return null;
+    private void loadDefaultStock() {
+        stockTransaction.addStock(store, new StockItem("A", 50));
+        stockTransaction.addStock(store, new StockItem("B", 30));
+        stockTransaction.addStock(store, new StockItem("C", 20));
+        stockTransaction.addStock(store, new StockItem("D", 15));
+        stockTransaction.addStockOffer(store, "A", new Offer(3, 130));
+        stockTransaction.addStockOffer(store, "B", new Offer(2, 45));
     }
 
-    private String validateAndStoreStock() {
-        return null;
-    }
+    private void clearScreen() {
+        try
+        {
+            final String os = System.getProperty("os.name");
 
-    private void registerStock() {
-
-    }
-
-    private void registerDefaultStock() {
-
-    }
-
-    private void addOfferToProduct() {
-
-    }
-
-    private void printScannedItems() {
-
+            if (os.contains("Windows"))
+            {
+                Runtime.getRuntime().exec("cls");
+            }
+            else
+            {
+                Runtime.getRuntime().exec("clear");
+            }
+        }
+        catch (final Exception e)
+        {
+            log.error("Unable to clear console");
+        }
     }
 }
